c6f21b9d27dc9e91a01e45cbf8f0ef1f2ed46723
napakalas <yuda.munarko@gmail.com>
2023-07-26 16:03:29 +1200

    tidy up test

--- examples/test.py
+++ examples/test.py
@@ -3,10 +3,7 @@ from npoexplorer import NPOExplorer
 
 if __name__ == '__main__':
     explorer = NPOExplorer()
-    # pprint(explorer.connectivity_models())
-    # pprint(explorer.entity_knowledge('ilxtr:NeuronSparcNlp'))
-    pprint(explorer.entity_knowledge('ilxtr:neuron-type-bromo-6'))
-    pprint(explorer.label('ILX:0775392'))
-    # pprint(explorer.entity_knowledge('ilxtr:neuron-type-aacar-11'))
-    # pprint(explorer.entity_knowledge('ilxtr:neuron-type-pancr-4'))
-    # pprint(explorer.label('ILX:0793667'))
+    pprint(explorer.connectivity_models())
+    pprint(explorer.entity_knowledge('ilxtr:NeuronKblad'))
+    pprint(explorer.entity_knowledge('https://apinatomy.org/uris/models/keast-bladder'))
+    pprint(explorer.entity_knowledge('ilxtr:neuron-type-keast-5'))
