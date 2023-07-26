15764d20c7d558fa04f0cad765ec49c15ed03278
napakalas <yuda.munarko@gmail.com>
2023-07-26 14:01:09 +1200

    adding partial order

--- npoexplorer/__init__.py
+++ npoexplorer/__init__.py
@@ -1,18 +1,49 @@
 #===============================================================================
 
 import stardog
-import requests
 import os
+import logging as log
+
+import ast
+import re
+import requests
+import rdflib
+from urllib.parse import urljoin
 
 #===============================================================================
 
 NPO_API_ENDPOINT = 'https://stardog.scicrunch.io:5821'
 NPO_USERNAME = os.environ.get('NPO_USERNAME')
 NPO_PASSWORD = os.environ.get('NPO_PASSWORD')
+DB_NAME = 'NPO'
+
+NPO_OWNER = 'SciCrunch'
+NPO_REPO = 'NIF-Ontology'
+NPO_BRANCH = 'neurons'
+
+NPO_DIR = 'ttl/generated/neurons'
+NPO_SOURCE = f'https://raw.githubusercontent.com/{NPO_OWNER}/{NPO_REPO}/{NPO_BRANCH}/'
+NPO_API = f"https://api.github.com/repos/{NPO_OWNER}/{NPO_REPO}/contents/{NPO_DIR}?ref={NPO_BRANCH}"
+NPO_FILES = {
+    'NPO': '../../npo.ttl',
+    'NEU_POP': 'apinatomy-neuron-populations.ttl',
+    'PARTIAL_ORDER': 'apinat-partial-orders.ttl',
+    'POPS_MORE': 'apinat-pops-more.ttl',
+    'SIMP_SHEET': 'apinat-simple-sheet.ttl',
+    'NLP': 'sparc-nlp.ttl',
+}
+
+NPO_PARTIAL_ORDER = 'apinat-partial-orders.ttl'
+
+
 
 #===============================================================================
 
 from npoexplorer.query import QUERIES, NAMESPACES
+
+#===============================================================================
+
+__version__ = '0.0.1'
 
 #===============================================================================
 
@@ -23,12 +54,89 @@ class NPOExplorer():
             'username': NPO_USERNAME,
             'password': NPO_PASSWORD,
         }
-        database_name = 'NPO'
-        self.__conn = stardog.Connection(database_name, **connection_details)
+        self.__conn = stardog.Connection(DB_NAME, **connection_details)
         self.__conn.begin()
         self.__connectivity_models = self.__get_connectivity_models()
         self.__labels = {}
+        # self.__load_npo_as_graph()
+        self.__load_npo_connectivities()
 
+        _, db_version = self.__select(QUERIES.DB_VERSION)
+        self.__metadata = {'SimpleSCKAN': db_version[0]['SimpleSCKAN']['value'], 
+                             'NPO': db_version[0]['NPO']['value']}
+        s_sckan_term = f'SimpleSCKAN built at {self.__metadata["SimpleSCKAN"]}'
+        npo_term = f'NPO built at {self.__metadata["NPO"]}'
+        log.info(f'NPO Explorer version {__version__} using {s_sckan_term} and {npo_term}')
+
+    def __load_npo_connectivities(self):
+        # loading partial connectivities from NPO repository
+        # due to unvailability in stardog
+        url = f'{NPO_SOURCE}{NPO_DIR}/{NPO_FILES["PARTIAL_ORDER"]}'
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                partial_order_text = response.text
+            else:
+                log.ERROR(f'Failed to load {NPO_FILES["PARTIAL_ORDER"]}. Status code: {response.status_code}')
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while fetching the file: {e}")
+
+        # a function to parse connectivities
+        def parse_connectivities(connectivities, sub_structure, root='blank'):
+            for sub_sub in sub_structure:
+                adj = (list(reversed(sub_sub[0]))[0], tuple(list(reversed(sub_sub[0]))[1:])) \
+                    if isinstance(sub_sub[0], list) else (sub_sub[0], ())
+                if root != ('blank', ()):
+                    connectivities += [(root, adj)]
+                if len(sub_sub) > 1:
+                    parse_connectivities(connectivities, sub_sub[1:], adj)
+
+        self.__connectivities = {}
+        for partial_order in partial_order_text.split('\n\n'):
+            if 'neuronPartialOrder' in partial_order:
+                connectivities = []
+                neuron, nested_structure = partial_order.split('ilxtr:neuronPartialOrder')
+                nested_structure = nested_structure.replace('.','')
+                # replace consecutive space with a single space
+                nested_structure = re.sub(r'\s+', ' ', nested_structure).strip()
+                # adding coma
+                pattern = r'\[([^]]+)\]'
+                def add_comma(match):
+                    elements = match.group(1).strip().split()
+                    return '[' + ', '.join(elements) + ']'
+                nested_structure = re.sub(pattern, add_comma, nested_structure)
+                # Quoting ILX and UBERON
+                pattern = r'(ILX:\d+|UBERON:\d+)'
+                nested_structure = re.sub(pattern, r'"\1"', nested_structure)        
+                # Specifying tuple
+                nested_structure = nested_structure.replace(' )', ', )').replace(' ( ', ', ( ')
+                # convert to tuple
+                conn_structure = ast.literal_eval(nested_structure)
+                # parse connectivities
+                connectivities = []
+                if conn_structure != 'blank':
+                    if len(conn_structure) > 1:
+                        
+                        root = (list(reversed(conn_structure[0]))[0], tuple(list(reversed(conn_structure[0]))[1:])) \
+                            if isinstance(conn_structure[0], list) else (conn_structure[0], ())
+                        parse_connectivities(connectivities, conn_structure[1:], root)
+                self.__connectivities[neuron.strip()] = connectivities  
+
+    def __load_npo_as_graph(self):
+        # this function is prepared to generate npo graph
+        # the graph will be useful when the need information is not available in stardog
+        self.__graph = rdflib.Graph()
+        for ttl_file in NPO_FILES.values():
+            try:
+                ttl_url =  urljoin(f'{NPO_SOURCE}{NPO_DIR}/', f'{ttl_file}')
+                self.__graph.parse(ttl_url, format='turtle')
+            except:
+                log.ERROR(f'Cannot load {ttl_file}')
+        from rdflib.namespace import Namespace
+        rdfs = Namespace(NAMESPACES.namespaces['rdfs'])
+        for subject, obj in graph.subject_objects(rdfs.label):
+            self.__labels[NAMESPACES.curie(str(subject))] = str(obj)
+            
     def __select(self, query):
         data = self.__conn.select(query)
         variables = data['head']['vars']
@@ -59,6 +167,61 @@ class NPOExplorer():
                 references.add(reference)
         return {'id':entity, 'label':entity, 'paths':list(paths), 'references':list(references)}
 
+    def __get_neuron_connectivities(self, entity):
+        # this function should be a standard method to get partial connectivity from stardog
+        # currently is not used
+        query = QUERIES.CONNECTIVITY.format(entity=entity)
+        _, results = self.__select(query)
+        connectivities = []
+        if len(results) > 0:
+            
+            self.__labels[results[0]['Region']['value']] = results[0]['Region_Label']['value'] if 'Region_Label' in results[0] else ''
+            self.__labels[results[0]['Layer']['value']] = results[0]['Layer_Label']['value'] if 'Layer_Label' in results[0] else ''
+            
+            for idx in range(1, len(results)):
+                prev_count = int(results[idx-1]['Count']['value'])
+                count = int(results[idx]['Count']['value'])
+                if count > prev_count:
+                    prev_layer = results[idx-1]['Layer']['value']
+                    prev_region = results[idx-1]['Region']['value']
+                    layer = results[idx]['Layer']['value']
+                    region = results[idx]['Region']['value']
+                    connectivities += [(
+                        (prev_region, (prev_layer,) if 'UBERON:' in prev_layer or 'ILX:' in prev_layer else ()),
+                        (region, (layer,) if 'UBERON:' in layer or 'ILX:' in layer else ()),
+                    )]
+                else:
+                    prev_idx = idx-1
+                    prev_count = int(results[prev_idx]['Count']['value'])
+                    while prev_count >= count:
+                        prev_idx = prev_idx-1
+                        prev_count = int(results[prev_idx]['Count']['value'])
+                    if prev_idx <= 0:
+                        continue
+                    else:
+                        prev_idx -= 1
+                        prev_layer = results[prev_idx]['Layer']['value']
+                        prev_region = results[prev_idx]['Region']['value']
+                        layer = results[idx]['Layer']['value']
+                        region = results[idx]['Region']['value']
+                        connectivities += [(
+                            (prev_region, (prev_layer,) if 'UBERON:' in prev_layer or 'ILX:' in prev_layer else ()),
+                            (region, (layer,) if 'UBERON:' in layer or 'ILX:' in layer else ()),
+                        )]
+
+                self.__labels[results[idx]['Region']['value']] = results[idx]['Region_Label']['value'] if 'Region_Label' in results[idx] else ''
+                self.__labels[results[idx]['Layer']['value']] = results[idx]['Layer_Label']['value'] if 'Layer_Label' in results[idx] else ''
+            
+        return connectivities
+    
+    def __get_connectivity_terms(self, entity):
+        query = QUERIES.CONNECTIVITY.format(entity=entity)
+        _, results = self.__select(query)
+        for rst in results:
+            self.__labels[rst['Region']['value']] = rst['Region_Label']['value'] if 'Region_Label' in rst else ''
+            self.__labels[rst['Layer']['value']] = rst['Layer_Label']['value'] if 'Layer_Label' in rst else ''
+            
+
     def __get_neuron_knowledge(self, entity):
         query = QUERIES.NEURON.format(entity=entity)
         _, results = self.__select(query)
@@ -69,8 +232,6 @@ class NPOExplorer():
                 nodes[obj] = {'region':[], 'layer':[]}
             if 'Region' in rst:
                 nodes[obj]['region'] += [rst['Region']['value']]
-            if 'Layer' in rst:
-                nodes[obj]['layer'] = [rst['Layer']['value']] + nodes[obj]['layer']
 
         def combine_layer_regions(nodes):
             combines = []
@@ -78,7 +239,6 @@ class NPOExplorer():
                 node = lr['region']+[obj]+lr['layer']
                 combines += [(node[0], tuple(node[1:])) if len(node)>1 else (node[0], ())]
             return combines
-
 
         somas, axons, dendrites, vias = {}, {}, {}, {}
         phenotypes, references, taxons, long_label = [], [], [], ''
@@ -117,18 +277,21 @@ class NPOExplorer():
                 self.__labels[rst['Layer']['value']] = rst['Layer_Label']['value'] if 'Layer_Label' in rst else ''
         
         # set neuron label
-        self.__labels['entity'] = long_label
+        self.__labels[entity] = long_label
 
         # map connectivity
         somas = combine_layer_regions(somas)
         axons = combine_layer_regions(axons)
         dendrites = combine_layer_regions(dendrites)
-        vias = combine_layer_regions(vias)
-        connectivities = [(soma, axon) for soma in somas for axon in axons]
+        
+        # connectivities = self.__get_neuron_connectivities(entity)
+        connectivities = self.__connectivities[entity]
+
+        # get required connectivity terms
+        self.__get_connectivity_terms(entity)
 
         return {
             'soma'          : somas,
-            'via'           : vias,
             'axons'         : axons,
             'connectivity'  : connectivities,
             'dendrites'     : dendrites,
@@ -140,52 +303,6 @@ class NPOExplorer():
             'references'    : references,
             'taxon'         : taxons
         }
-
-    
-    # def __get_neuron_knowledge(self, entity):
-    #     # get connectivities
-    #     query = QUERIES.NEURON_CONNECTIVITIES.format(entity=entity)
-    #     _, results = self.__select(query)
-    #     axons, connectivities = set(), {}
-    #     for result in results:
-    #         # get axon: (region, axon) if region available; (axon,) if not
-    #         if 'B_Region_IRI' in result:
-    #             axons.add((NAMESPACES.curie(result['B_Region_IRI']['value']), NAMESPACES.curie(result['B_IRI']['value'])))
-    #         else:
-    #             axons.add((NAMESPACES.curie(result['B_IRI']['value'],)))
-    #         # get connectitity
-    #         connect = (NAMESPACES.curie(result['A_IRI']['value']), NAMESPACES.curie(result['B_IRI']['value']))
-    #         if connect not in connectivities:
-    #             connectivities[connect] = []
-    #         if 'C_IRI' in result:
-    #             connectivities[connect] += [NAMESPACES.curie(result['C_IRI']['value'])]
-    #     # convert connectivities
-    #     connectivities= [(a_to_b, tuple(via_c)) for a_to_b, via_c in connectivities.items()]
-    #     n_pop = {'axons':list(axons), 'connectivity':connectivities, 
-    #              'dendrites':[], 'errors':[], 'id':entity, 'label':entity, 'long-label':'', 
-    #              'phenotypes':[], 'references':[], 'taxon':''}
-
-    #     # get metadata
-    #     query = QUERIES.NEURON_META.format(entity=entity)
-    #     _, results = self.__select(query)
-    #     if len(results) > 0:
-    #         result = results[0]
-    #         if 'Dendrite_Location_IRIs' in result:
-    #             n_pop['dendrites'] = [NAMESPACES.curie(dend)for dend in result['Dendrite_Location_IRIs']['value'].split(',')]
-    #         n_pop['long-label'] = result['Neuron_Label']['value']
-    #         if 'Phenotype_IRIs' in result:
-    #             n_pop['phenotypes'] = [NAMESPACES.curie(dend)for dend in result['Phenotype_IRIs']['value'].split(',')]
-    #         if 'Reference' in result:
-    #             n_pop['references'] = result['Reference']['value']
-    #         if 'Species_IRIs' in result:
-    #             n_pop['taxon'] = [NAMESPACES.curie(dend)for dend in result['Species_IRIs']['value'].split(',')]
-
-                
-
-
-
-    #    return n_pop
-        
 
     def connectivity_models(self):
         return self.__connectivity_models
@@ -203,6 +320,12 @@ class NPOExplorer():
     def label(self, entity):
         return self.__labels[entity]
     
+    def metadata(self, name=None):
+        if name==None:
+            return self.__metadata
+        elif name in self.__metadata:
+            return self.__metadata[name]
+
     def close(self):
             self.__conn.close()
 
--- npoexplorer/query.py
+++ npoexplorer/query.py
@@ -1,4 +1,3 @@
-
 class NAMESPACES:
     namespaces = {
         'ilxtr'     : 'http://uri.interlex.org/tgbugs/uris/readable/',
@@ -6,6 +5,7 @@ class NAMESPACES:
         'rdfs'      : 'http://www.w3.org/2000/01/rdf-schema#',
         'UBERON'    : 'http://purl.obolibrary.org/obo/UBERON_',
         'NCBITaxon' : 'http://purl.obolibrary.org/obo/NCBITaxon_',
+        'rdf'       : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     }
 
     @staticmethod
@@ -59,167 +59,59 @@ class QUERIES:
     }}
     """
 
-    PHENOTYPES = """
-        SELECT DISTINCT  ?Phenotype_IDs
-            (group_concat(distinct ?Phenotype_ID; separator=",") as ?Phenotype_IDs) 
-            WHERE                  
-            {{
-                VALUES(?Neuron_IRI){{({entity})}}
-                ?Neuron_IRI rdfs:subClassOf*/rdfs:label 'Neuron'. #http://uri.neuinfo.org/nif/nifstd/sao1417703748   
-                
-                ?Neuron_IRI ilxtr:hasSomaLocation ?A_IRI.
-                OPTIONAL {{?Neuron_IRI ilxtr:hasAxonLocation ?C_IRI.}}
-                ?Neuron_IRI (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
-                OPTIONAL {{
-                    ?Neuron_IRI (
-                        ilxtr:hasNeuronalPhenotype | 
-                        ilxtr:hasFunctionalCircuitRole |
-                        ilxtr:hasCircuitRole |
-                        ilxtr:hasProjection  
-                    ) ?Phenotype_ID.
-                }}
-            }}
-    """
-
     # Query: This query is for loading all object related to a subject
     # This is sufficient to obtain information about a population of neurons. 
     # The identification of the next attribute is at the code level.
+    # NEURON = """
+    #     SELECT DISTINCT ?Neuron_IRI ?Predicate ?Object ?Object_Label ?Region ?Region_Label ?Layer ?Layer_Label {{
+    #         VALUES(?Neuron_IRI){{({entity})}}
+    #         ?Neuron_IRI ?Predicate ?Object.
+    #         OPTIONAL{{?Object rdfs:label ?Object_Label}}
+    #         OPTIONAL{{
+    #             ?_1 ?Object ?Region .
+    #             OPTIONAL{{?Region rdfs:label ?Region_Label .}}
+    #         }}
+    #         OPTIONAL{{
+    #             ?Neuron_IRI (
+    #                 ilxtr:hasSomaLocation|
+    #                 ilxtr:hasAxonTerminalLocation|
+    #                 ilxtr:hasAxonSensoryLocation|
+    #                 ilxtr:hasDendriteLocation|
+    #                 ilxtr:hasAxonLocation
+    #                 ) ?Object .
+    #             ?_2 ?Layer ?Object .
+    #             OPTIONAL{{?Layer rdfs:label ?Layer_Label .}}
+    #             FILTER(STRSTARTS(STR(?Layer), STR(UBERON:)) || STRSTARTS(STR(?Layer), STR(ILX:)))
+    #         }}
+    #     }}
+    # """
     NEURON = """
-        SELECT DISTINCT ?Neuron_IRI ?Predicate ?Object ?Object_Label ?Region ?Region_Label ?Layer ?Layer_Label {{
+        SELECT DISTINCT ?Neuron_IRI ?Predicate ?Object ?Object_Label {{
             VALUES(?Neuron_IRI){{({entity})}}
             ?Neuron_IRI ?Predicate ?Object.
             OPTIONAL{{?Object rdfs:label ?Object_Label}}
-            OPTIONAL{{
-                ?_ ?Object ?Region .
-                OPTIONAL{{?Region rdfs:label ?Region_Label .}}
-            }}
-            OPTIONAL{{
-                ?Neuron_IRI (
-                    ilxtr:hasSomaLocation|
-                    ilxtr:hasAxonTerminalLocation|
-                    ilxtr:hasAxonSensoryLocation|
-                    ilxtr:hasDendriteLocation|
-                    ilxtr:hasAxonLocation
-                    ) ?Object .
-                ?_2 ?Layer ?Object .
-                OPTIONAL{{?Layer rdfs:label ?Layer_Label .}}
-                FILTER(STRSTARTS(STR(?Layer), STR(UBERON:)) || STRSTARTS(STR(?Layer), STR(ILX:)))
-            }}
         }}
     """
 
-    # QUERY: This query is for loading the a-b-via-c results in sckan-explorer.
-    # Neuron populations where A projects to B via some Nerve C
-    NEURON_CONNECTIVITIES = """
-        SELECT DISTINCT ?A_IRI ?A_Label ?B_IRI ?B_Label ?B_Region_IRI ?C_IRI ?C_Label ?Target_Organ_IRI ?Target_Organ_Label
-        {{
+    CONNECTIVITY = """
+        SELECT ?Layer ?Layer_Label ?Region ?Region_Label (COUNT(?d) AS ?Count) WHERE{{
             VALUES(?Neuron_IRI){{({entity})}}
-            ?Neuron_IRI ilxtr:hasSomaLocation ?A_IRI.
-            ?A_IRI rdfs:label ?A_Label.
-        
-            OPTIONAL {{
-                ?Neuron_IRI ilxtr:hasAxonLocation ?C_IRI. 
-                ?C_IRI rdfs:label ?C_Label.
-            }}
-            
-            ?Neuron_IRI (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
-            ?B_IRI (rdfs:label) ?B_Label.
-
-            OPTIONAL{{
-                ?B_IRI rdfs:subClassOf+ [rdf:type owl:Restriction ;
-                                            owl:onProperty partOf: ; 
-                                            owl:someValuesFrom ?Target_Organ_IRI].
-                ?Target_Organ_IRI rdfs:label ?Target_Organ_Label     
-                FILTER (?Target_Organ_Label in ( 'heart', 'ovary', 'brain', 'urethra', 'esophagus', 'skin of body', 'lung', 'liver', 
-                                            'lower urinary tract', 'urinary tract', 'muscle organ','gallbladder', 'colon', 'kidney', 
-                                            'large intestine' ,'small intestine', 'stomach', 'spleen', 'urinary bladder', 
-                                            'penis', 'clitoris', 'pancreas'))
-                
-            }}
-
-            OPTIONAL{{
-                ?_TMP ?B_IRI ?B_Region_IRI .
-            }}
-        }}
-        ORDER BY ?Neuron_IRI ?A_Label ?B_IRI ?C_Label
+            ?Neuron_IRI ilxtr:neuronPartialOrder ?o.
+            ?o (rdf:first|rdf:rest)* ?d .
+            ?d (rdf:first|rdf:rest)* ?e .
+            ?e ?Layer ?Region .
+            OPTIONAL{{?Layer rdfs:label ?Layer_Label}}
+            OPTIONAL{{?Region rdfs:label ?Region_Label}}
+            FILTER (REGEX(STR(?Region), STR(ILX:)) || REGEX(STR(?Region), STR(UBERON:)))
+            FILTER (?Layer=rdf:first || REGEX(STR(?Layer), STR(ILX:)) || REGEX(STR(?Layer), STR(UBERON:)))
+        }} GROUP BY ?Region ?Layer ?Region_Label ?Layer_Label ?e
     """
 
-    NEURON_META = """
-        SELECT DISTINCT ?Neuron_IRI ?Neuron_Label ?Neuron_Pref_Label ?Species_IRIs ?Species ?Sex_IRI ?Sex 
-                ?Phenotype_IRIs ?Forward_Connections ?Dendrite_Location_IRIs ?Alert ?Reference
-        WHERE
-        {{
-            VALUES(?Neuron_IRI){{({entity})}}
-            {{ 
-                SELECT DISTINCT  ?Neuron_IRI ?Neuron_Label ?Neuron_Pref_Label ?Sex_IRI ?Sex ?Alert ?Reference
-                WHERE                  
-                {{
-                    OPTIONAL{{?Neuron_IRI rdfs:label ?Neuron_Label.}}
-                    OPTIONAL{{?Neuron_IRI skos:prefLabel ?Neuron_Pref_Label.}}
-                    ?Neuron_IRI ilxtr:hasSomaLocation ?A_IRI.
-                    ?Neuron_IRI (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
-                    OPTIONAL {{
-                        ?Neuron_IRI ilxtr:hasPhenotypicSex/rdfs:label ?Sex.
-                        ?Neuron_IRI ilxtr:hasPhenotypicSex ?Sex_IRI.
-                    }}
-                    OPTIONAL {{?Neuron_IRI ilxtr:reference ?Reference.}}
-                    OPTIONAL {{?Neuron_IRI ilxtr:alertNote ?Alert.}}
-                }}
-            }}
-            
-            {{ 
-                SELECT DISTINCT ?Neuron_IRI
-                (group_concat(distinct ?ObservedIn; separator=",") as ?Species) 
-                (group_concat(distinct ?Species_IRI; separator=",") as ?Species_IRIs) 
-                WHERE                  
-                {{
-                    ?Neuron_IRI ilxtr:hasSomaLocation ?A_IRI.
-                    ?Neuron_IRI (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
-                    OPTIONAL {{
-                        ?Neuron_IRI ilxtr:isObservedInSpecies/rdfs:label ?ObservedIn.
-                        ?Neuron_IRI ilxtr:isObservedInSpecies ?Species_IRI
-                    }}
-                }}
-                GROUP BY ?Neuron_IRI
-            }}
-            {{
-                SELECT DISTINCT ?Neuron_IRI
-                (group_concat(distinct ?ForwardConnection; separator=",") as ?Forward_Connections)  
-                WHERE                  
-                {{
-                    ?Neuron_IRI ilxtr:hasSomaLocation ?A_IRI.
-                    ?Neuron_IRI (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
-                    OPTIONAL {{?Neuron_IRI ilxtr:hasForwardConnection ?ForwardConnection.}}
-                }}
-                GROUP BY ?Neuron_IRI
-            }}
-
-            {{   
-                SELECT DISTINCT ?Neuron_IRI  
-                (group_concat(distinct ?Phenotype_IRI; separator=",") as ?Phenotype_IRIs) 
-                WHERE                  
-                {{
-                    ?Neuron_IRI ilxtr:hasSomaLocation ?A_IRI.
-                    ?Neuron_IRI (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
-                    OPTIONAL {{?Neuron_IRI (ilxtr:hasNeuronalPhenotype | 
-                                        ilxtr:hasFunctionalCircuitRole |
-                                        ilxtr:hasCircuitRole |
-                                        ilxtr:hasProjection  
-                                        ) ?Phenotype_IRI.}}
-                }}
-                GROUP BY ?Neuron_IRI
-            }}
-
-            {{
-                SELECT DISTINCT ?Neuron_IRI 
-                (group_concat(distinct ?Dendrite_Location_IRI; separator=",") as ?Dendrite_Location_IRIs) 
-                WHERE                  
-                {{
-                    ?Neuron_IRI ilxtr:hasSomaLocation ?A_IRI.
-                    ?Neuron_IRI (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
-                    OPTIONAL {{?Neuron_IRI ilxtr:hasDendriteLocation ?Dendrite_Location_IRI}}
-                }}
-                GROUP BY ?Neuron_IRI
-            }}
+    DB_VERSION = """
+        PREFIX TTL: <https://raw.githubusercontent.com/SciCrunch/NIF-Ontology/neurons/ttl/>
+        SELECT DISTINCT ?NPO ?SimpleSCKAN WHERE{{
+            OPTIONAL{{TTL:npo.ttl owl:versionInfo ?NPO.}}
+            OPTIONAL{{TTL:simple-sckan.ttl owl:versionInfo ?SimpleSCKAN.}}
         }}
+
     """
